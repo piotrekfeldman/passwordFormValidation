e19d98ad7250fd41a7fa1819a7d0e3ed58bdd311
Piotr Feldman <piotr.feldman@smart-points.pl>
2021-11-03 19:22:40 +0100

    solution

---
+++ script.js
@@ -0,0 +1,81 @@
+let firstPassword = document.getElementById("password");
+let secondPassword = document.getElementById("retype-password");
+
+firstPassword.onfocus = function() {
+    document.getElementById("message").style.display = "block";
+  }
+
+  firstPassword.onblur = function() {
+    document.getElementById("message").style.display = "none";
+  }
+
+  secondPassword.onfocus = function() {
+    document.getElementById("passwordNotTheSame").style.display = "block";
+  }
+
+firstPassword.onkeyup = function() {
+    var lowerCaseLetters = /[a-z]/g;
+    if(firstPassword.value.match(lowerCaseLetters)) {
+        letter.classList.remove("invalid");
+        document.querySelector('#letter').style.display = "none";
+    } else {
+        document.querySelector('#letter').style.display = "block";
+        letter.classList.add("invalid");
+  }
+    var upperCaseLetters = /[A-Z]/g;
+    if(firstPassword.value.match(upperCaseLetters)) {
+
+      capital.classList.remove("invalid");
+      document.querySelector('#capital').style.display = "none";
+
+    } else {
+      document.querySelector('#capital').style.display = "block";
+      capital.classList.add("invalid");
+    }
+  
+    var numbers = /[0-9]/g;
+    if(firstPassword.value.match(numbers)) {
+      number.classList.remove("invalid");
+      document.querySelector('#number').style.display = "none";
+
+    } else {
+        document.querySelector('#number').style.display = "block";
+        number.classList.add("invalid");
+    }
+
+    if(firstPassword.value.length >= 8) {
+    length1.classList.remove("invalid");
+    document.querySelector('#length1').style.display = "none";
+
+  } else {
+    document.querySelector('#length1').style.display = "block";
+    length1.classList.add("invalid");
+  }
+
+  if(firstPassword.value.length >= 8 && (firstPassword.value.match(numbers)) && (firstPassword.value.match(upperCaseLetters)) 
+  && (firstPassword.value.match(lowerCaseLetters))) {
+    paragraph.classList.remove("invalid");
+    document.querySelector('#paragraph').style.display = "none";
+
+  }
+}
+
+secondPassword.onkeyup = function() {
+  if(!(secondPassword.value.match(firstPassword.value))){
+    passwordNotTheSame.classList.add("invalid");
+    document.querySelector('#messageConfirmPassword').style.display = "block";
+  }  else {      
+    document.querySelector('#messageConfirmPassword').style.display = "none";
+    passwordNotTheSame.classList.remove("invalid");
+  }
+
+  if(firstPassword.value.length !== secondPassword.value.length) {
+    passwordNotTheSame.classList.add("invalid");
+    document.querySelector('#messageConfirmPassword').style.display = "block";
+  }
+
+  if(firstPassword.value.length === 0) {
+    passwordNotTheSame.classList.remove("invalid");
+    document.querySelector('#messageConfirmPassword').style.display = "none";
+  }
+}
